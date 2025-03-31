3f903501baf80a102f0eaa3c5cd2d93a0c6a606f
AndranikA <andranika93@gmail.com>
2025-04-01 00:37:17 +0400

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ logger.setLevel(logging.INFO)
 
 # Конфигурация
 class Config:
-    BOT_TOKEN = "7661688763:AAFxtkUpEDEmeUTnqcDceUpGsuj1FpSxbP0"  # os.getenv("BOT_TOKEN")
+    BOT_TOKEN = "7661688763:AAEyO99QmNxZ8UPtL0OzqISBQ0SmcSqWMiQ"  # os.getenv("BOT_TOKEN")
     RENDER_APP_URL = "https://einstein-point-bot-7k8m.onrender.com"  # os.getenv("RENDER_APP_URL")
     DB_NAME = "points_bot.db"
     BACKUP_CHAT_ID = -1002571801416  # ID чата для бэкапов(сохранении данных)
